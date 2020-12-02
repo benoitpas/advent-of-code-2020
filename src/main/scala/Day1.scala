de7f8ef2f438d4b055c5f13f4a15fe0fba602217
benoitpas <bpasquereau@gmail.com>
2020-12-02 08:10:35 +0000

    Day 1 part 2

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -5,16 +5,33 @@ object Day1 {
    
   val input = Source.fromResource("day1-input.txt").getLines
 
+  val totalSum = 2020
+
   // Interesting find: 'Stream' has been deprecated and replaced by 'LazyList'
   def findPair(numbers : List[Int]) = {
     val pairs = for(n1 <- numbers.to(LazyList);n2<-numbers.to(LazyList)) yield (n1,n2)
     
-    pairs.filter((p) => p._1+p._2 == 2020).take(1).toList
+    pairs.filter((p) => p._1+p._2 == totalSum).take(1).toList
   }
+
+  def findTriple(numbers : List[Int]) = {
+    val pairs = for(
+      n1 <- numbers.to(LazyList);
+      n2<-numbers.to(LazyList);
+      n3<-numbers.to(LazyList)
+      ) yield (n1,n2,n3)
+    
+    pairs.filter((t) => t._1+t._2+t._3 == totalSum).take(1).toList
+  }
+
 
   def main(args: Array[String]): Unit = {
     val numbers = for(i <- input.toList) yield i.toInt
+
     val pair = findPair(numbers)(0)
-    println(pair._1 * pair._2)
+    println(s"Pair product: ${pair._1 * pair._2}")
+
+    val triple = findTriple(numbers)(0)
+    println(s"Triple product: ${triple._1 * triple._2 * triple._3}")
   }
 }
--- src/test/scala/Day1Test.scala
+++ src/test/scala/Day1Test.scala
@@ -3,9 +3,15 @@ import org.junit.Test
 import org.junit.Assert._
 
 class Day1Test {
-  @Test def test(): Unit = {
-    val numbers = List(1721, 979, 366, 299, 675, 1456)
+  val numbers = List(1721, 979, 366, 299, 675, 1456)
+
+  @Test def testPair(): Unit = {
     val pair = Day1.findPair(numbers)(0)
     assertEquals(514579, pair._1 * pair._2)
+  }
+
+  @Test def testTriple(): Unit = {
+    val triple = Day1.findTriple(numbers)(0)
+    assertEquals(241861950, triple._1 * triple._2 * triple._3)
   }
 }
