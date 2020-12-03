fd9afd23bf648a822411f2e9dc30cf51dc46f6a8
benoitpas <bpasquereau@gmail.com>
2020-12-03 21:57:50 +0000

    Day 3 part 2

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -17,16 +17,23 @@ object Day3 {
     "#...##....#",
     ".#..#...#.#").toIndexedSeq
 
-    def countTrees(terrain : Seq[String], right: Int, down: Int) = {
-      val p = (1 to terrain.length-1) map ( i => (i * right, i * down))
-      p.map((x,y) => {
+    def countTrees(terrain:Seq[String], directions: List[(Int,Int)]) : Long  = 
+      directions.map((right,down) => countTrees(terrain, right, down)).reduce( _ * _)
+
+    def countTrees(terrain : Seq[String], right: Int, down: Int) : Long = {
+      val y = (down to terrain.length-1 by down) 
+      val x = (1 to y.length).map(_ * right)
+
+      (x zip y).map((x,y) => {
         val row = terrain(y)
         row(x % row.length)
       }).filter(_ == '#').length
     } 
 
+    val directions = List((1,1),(3,1),(5,1),(7,1),(1,2))
 
     def main(args: Array[String]): Unit = {
       println("part1="+countTrees(terrain2,3,1))
+      println("part2="+countTrees(terrain2,directions))
     }
 }
--- src/test/scala/Day3Test.scala
+++ src/test/scala/Day3Test.scala
@@ -3,11 +3,36 @@ import org.junit.Assert._
 
 class Day3Test {
   @Test def testPart1a(): Unit = {
-    assertEquals(7, Day3.countTrees(Day3.terrain1,3,1))
+    assertEquals(7, Day3.countTrees(Day3.terrain1,3,1).toInt)
   }
 
   @Test def testPart1b(): Unit = {
-    assertEquals(195, Day3.countTrees(Day3.terrain2,3,1))
+    assertEquals(195, Day3.countTrees(Day3.terrain2,3,1).toInt)
   }
+
+  @Test def testPart2a(): Unit = {
+    assertEquals(2, Day3.countTrees(Day3.terrain1,1,1).toInt)
+  }
+
+  @Test def testPart2b(): Unit = {
+    assertEquals(3, Day3.countTrees(Day3.terrain1,5,1).toInt)
+  }
+
+  @Test def testPart2c(): Unit = {
+    assertEquals(4, Day3.countTrees(Day3.terrain1,7,1).toInt)
+  }
+
+  @Test def testPart2d(): Unit = {
+    assertEquals(2, Day3.countTrees(Day3.terrain1,1,2).toInt)
+  }
+
+  @Test def testPart2e(): Unit = {
+    assertEquals(336, Day3.countTrees(Day3.terrain1, Day3.directions).toInt)
+  }
+
+  @Test def testPart2f(): Unit = {
+    assertEquals(3772314000L, Day3.countTrees(Day3.terrain2, Day3.directions))
+  }
+
 
 }
