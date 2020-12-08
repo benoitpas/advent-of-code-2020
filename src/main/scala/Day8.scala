551a76afe6a3510ea803041c19c0fb785bcaaf44
benoitpas <bpasquereau@gmail.com>
2020-12-08 19:43:32 +0000

    Day 8 part2

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -14,28 +14,51 @@ object Day8 {
     "jmp -4",
     "acc +6")
 
-    def parse(instructions:Array[String]) = instructions.map(_.split(" ").toList match {
-      case instruction::operand::Nil => (instruction, operand.toInt)
-    })
+  def parse(instructions:Array[String]) = instructions.map(_.split(" ").toList match {
+    case instruction::operand::Nil => (instruction, operand.toInt)
+  })
 
-    def run(instructions:Array[String]) = {
+  def run(instructions:Array[(String,Int)]): (Int,Int) = {
 
-      val pInstructions = parse(instructions)
-      import scala.annotation.tailrec
-      @tailrec
-      def loop(pc:Int, acc:Int, previousPc:Set[Int]) : Int =
-        if (!previousPc.contains(pc)) {
-          pInstructions(pc) match {
-            case ("acc",inc) => loop(pc+1, acc+inc, previousPc + pc)
-            case ("jmp",inc) => loop(pc+inc, acc, previousPc + pc)
-            case ("nop",_) => loop(pc+1, acc, previousPc + pc)
+    import scala.annotation.tailrec
+    @tailrec
+    def loop(pc:Int, acc:Int, previousPc:Set[Int]) : (Int,Int) =
+      if (!previousPc.contains(pc) && pc < instructions.length) {
+        instructions(pc) match {
+          case ("acc",inc) => loop(pc+1, acc+inc, previousPc + pc)
+          case ("jmp",inc) => loop(pc+inc, acc, previousPc + pc)
+          case ("nop",_) => loop(pc+1, acc, previousPc + pc)
+         }
+      } else (pc,acc)
+
+    loop(0, 0, Set())
+  }
+
+  def run(instructions:Array[String]): (Int,Int) = run(parse(instructions))
+
+
+  def patch(instructions:Array[String]) = {
+    val pInstructions = parse(instructions)
+
+    (pInstructions.length -1 to 0 by -1).to(LazyList).flatMap( (i) => {
+      val mInstructions = pInstructions.clone
+      mInstructions(i) match {
+        case ("nop", op) => {
+          mInstructions(i) = ("jmp",op)
+          List(run(mInstructions))
           }
-        } else acc
+        case ("jmp", op) => {
+          mInstructions(i) = ("nop",op)
+          List(run(mInstructions))
+          }
+        case _ => List()
+      }
 
-      loop(0, 0, Set())
-    }
+    }).filter(_._1 == pInstructions.length).take(1).toList
+  }
 
-    def main(args: Array[String]): Unit = {
-      println("part1="+run(allInstructions))
-    }
+  def main(args: Array[String]): Unit = {
+    println("part1="+run(allInstructions)._2)
+    println("part2="+patch(allInstructions)(0)._2)
+  }
 }
--- src/test/scala/Day8Test.scala
+++ src/test/scala/Day8Test.scala
@@ -5,11 +5,18 @@ import Day8._
 
 class Day8Test {
   @Test def testPart1a(): Unit = {    
-    assertEquals(5, run(instructions1))
+    assertEquals(5, run(instructions1)._2)
   }
 
   @Test def testPart1b(): Unit = {    
-    assertEquals(2058, run(allInstructions1))
+    assertEquals(2058, run(allInstructions)._2)
   }
 
+  @Test def testPart2a(): Unit = {    
+    assertEquals(8, patch(instructions1)(0)._2)
+  }
+
+  @Test def testPart2b(): Unit = {    
+    assertEquals(1000, patch(allInstructions)(0)._2)
+  }
 }
