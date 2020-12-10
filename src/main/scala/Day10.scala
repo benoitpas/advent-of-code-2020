b5bf377b2aa25d4c9783feddc647889ade752ebe
benoitpas <bpasquereau@gmail.com>
2020-12-10 21:18:34 +0000

    Day 10 part 2

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -2,6 +2,18 @@ object Day10 {
   import scala.io.Source
   val fullInput = Source.fromResource("day10-input.txt").getLines.toList.map(_.toInt)
 
+  val verySmallInput = List(16,
+    10,
+    15,
+    5,
+    1,
+    11,
+    7,
+    19,
+    6,
+    12,
+    4)
+
   val smallInput = List(28,
     33,
     18,
@@ -42,9 +54,48 @@ object Day10 {
 
     countDifferences(1) * countDifferences(3)
   }
+/*
+  def countCombinations(adapters:List[Int]) = {
+    val l = adapters.sorted
+    val l2 = 0::l.appended(l.last+3)
+    def countCombinationsRec(l:List[Int]) : Long =
+      l match {
+        case h1::h2::h3::h4::tail => (countCombinationsRec(h2::h3::h4::tail)
+          + (if ((h3 - h1) <= 3) countCombinationsRec(h3::h4::tail) else 0)
+          + (if ((h4 - h1) <= 3) countCombinationsRec(h4::tail) else 0))
+        case h1::h2::h3::Nil =>  1L + (if ((h3 - h1) <= 3) 1L else 0)
+        case h1::h2::Nil => 1 // h2, the last one should always h2=h1+3
+        case h::Nil => 1L
+        case Nil => 0L
+      }
+    countCombinationsRec(l2)
+  }
+  */
+
+  def countCombinations(adapters:List[Int]) = {
+    val l = adapters.sorted
+    val l2 = 0::l.appended(l.last+3)
+    val counts = l2.foldRight(List[(Int,Long)]())((elem,acc) => acc match {
+      case Nil => List((elem, 1L))
+      case h::Nil => (elem, h._2)::h::Nil
+      case h1::h2::Nil => {
+        val inc = if ((h2._1 - elem) < 3) h2._2 else 0
+        (elem,h1._2 + inc)::h1::h2::Nil
+      }
+      case h1::h2::h3::tail => {
+        val inc = 
+          (if ((h2._1 - elem) <= 3) h2._2 else 0) +
+          (if ((h3._1 - elem) <= 3) h3._2 else 0)
+        (elem,h1._2 + inc)::h1::h2::h3::tail
+      }
+      case h::tail => (elem,h._2+1)::h::tail
+    })
+    counts.head._2
+  }
+
 
   def main(args: Array[String]): Unit = {
-    println(joltDifferencesMultiplied(smallInput))    
-    println(joltDifferencesMultiplied(fullInput))    
+    println("part1="+joltDifferencesMultiplied(fullInput))
+    println("part2="+countCombinations(fullInput)) 
   }
 }
--- src/test/scala/Day10Test.scala
+++ src/test/scala/Day10Test.scala
@@ -5,11 +5,28 @@ import Day10._
 
 class Day10Test {
   @Test def testPart1a(): Unit = {
+    assertEquals(35, joltDifferencesMultiplied(verySmallInput))
+  }
+  
+  @Test def testPart1b(): Unit = {
     assertEquals(220, joltDifferencesMultiplied(smallInput))
   }
 
-  @Test def testPart1b(): Unit = {
+  @Test def testPart1c(): Unit = {
     assertEquals(2368, joltDifferencesMultiplied(fullInput))
   }
+
+  @Test def testPart2a(): Unit = {
+    assertEquals(8, countCombinations(verySmallInput))
+  }
+  
+  @Test def testPart2b(): Unit = {
+    assertEquals(19208, countCombinations(smallInput))
+  }
+
+  @Test def testPart2c(): Unit = {
+    assertEquals(1727094849536L, countCombinations(fullInput))
+  }
+
 
 }
