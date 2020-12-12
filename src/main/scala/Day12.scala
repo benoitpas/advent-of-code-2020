0472bf1a365c4af1bfd3d27825892d1af5ac1930
benoitpas <bpasquereau@gmail.com>
2020-12-12 12:19:06 +0000

    Day 12 part 2

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -9,7 +9,7 @@ object Day12 {
   "F11")
 
   type Position = (Int, Int)
-  type State = (Char, Position)
+  type State1 = (Char, Position)
 
   def go(position:Position, direction:Char, v:Int) : Position = 
     direction match
@@ -18,8 +18,10 @@ object Day12 {
       case 'E' => (position._1 + v, position._2)
       case 'W' => (position._1 - v, position._2)
 
+  val clockWiseDirection = List('N', 'E', 'S', 'W')
+  val headings = clockWiseDirection.toSet
+
   def rotate(direction: Char, angle:Int, turn:Char) =
-    val clockWiseDirection = List('N', 'E', 'S', 'W')
     val clockWise = (turn match {
       case 'L' => -1
       case 'R' => 1
@@ -29,7 +31,7 @@ object Day12 {
     val nextIndex = (index + clockWise +nbDirection) % nbDirection
     clockWiseDirection(nextIndex)
 
-  def next(state:State, cmd:String) = 
+  def next1(state:State1, cmd:String) = 
     val p = cmd.splitAt(1)
     (p._1(0), p._2.toInt, state._1) match
       case ('F', v, d) => (d, go(state._2, d, v))
@@ -40,13 +42,59 @@ object Day12 {
       case ('R', a, d) => (rotate(d, a, 'R'), state._2)
       case ('L', a, d) => (rotate(d, a, 'L'), state._2)
 
-  val iPos = State('E',(0,0))
+  val iPos1 = State1('E',(0,0))
 
-  def iterate(input:List[String]) =
-    val lastState = input.foldLeft(iPos)(next)
+  def iterate1(input:List[String]) =
+    val lastState = input.foldLeft(iPos1)(next1)
     math.abs(lastState._2._1) + math.abs(lastState._2._2)
 
+
+  case class State2(ship: Position, waypoint: Position) {
+    def moveShip(v:Int) = State2(
+      Position(
+        ship._1 + waypoint._1 * v,
+        ship._2 + waypoint._2 * v),
+      waypoint)
+
+    def moveWaypoint(h: Char, v:Int) = State2(
+      ship,
+      h match {
+        case 'N' => Position(waypoint._1, waypoint._2 - v)
+        case 'S' => Position(waypoint._1, waypoint._2 + v)
+        case 'E' => Position(waypoint._1 - v, waypoint._2)
+        case 'W' => Position(waypoint._1 + v, waypoint._2)
+      }
+    )
+
+    def rotateWaypoint(v: Int) = State2(
+      ship,
+      v match {
+        case  -90 => (-waypoint._2,  waypoint._1)
+        case   90 => ( waypoint._2, -waypoint._1)
+        case  180 => (-waypoint._1, -waypoint._2)
+        case -180 => (-waypoint._1, -waypoint._2)
+        case -270 => ( waypoint._2, -waypoint._1)
+        case  270 => (-waypoint._2,  waypoint._1)
+      }
+    )
+  }
+
+  val iPos2 = State2((0,0),(-10,-1))
+
+  def next2(state:State2, cmd:String) = 
+    val (cmdLetter, cmdValue) = cmd.splitAt(1)
+    (cmdLetter(0), cmdValue.toInt) match
+      case ('F',v) => state.moveShip(v)
+      case (heading,v) if (headings.contains(heading)) => state.moveWaypoint(heading, v)
+      case ('L', v) => state.rotateWaypoint(-v)
+      case ('R', v) => state.rotateWaypoint(v)
+
+  def iterate2(input:List[String]) =
+    val lastState = input.foldLeft(iPos2)(next2)
+    math.abs(lastState.ship._1) + math.abs(lastState.ship._2)
+    
   def main(args: Array[String]): Unit = {
-    println("part1=" + iterate(fullInput))
+    println("part1=" + iterate1(fullInput))
+    println("part1=" + iterate2(fullInput))
   }
 }
--- src/test/scala/Day12Test.scala
+++ src/test/scala/Day12Test.scala
@@ -5,10 +5,19 @@ import Day12._
 
 class Day12Test {
   @Test def testPart1a(): Unit = {
-    assertEquals(25, iterate(smallInput))
+    assertEquals(25, iterate1(smallInput))
   }
   
   @Test def testPart1b(): Unit = {
-    assertEquals(1148, iterate(fullInput))
+    assertEquals(1148, iterate1(fullInput))
   }
+
+  @Test def testPart2a(): Unit = {
+    assertEquals(286, iterate2(smallInput))
+  }
+  
+  @Test def testPart2b(): Unit = {
+    assertEquals(1148, iterate2(fullInput))
+  }
+
 }
