419ec4e61b31fc493b2246b9fcdb7bacdefe9696
benoitpas <bpasquereau@gmail.com>
2020-12-13 19:55:49 +0000

    Day 13 part 2

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -2,11 +2,11 @@ object Day13 {
   import scala.io.Source
   val fullInput = Source.fromResource("day13-input.txt").getLines.toList
 
-  val smallInput = List("939",
-    "7,13,x,x,59,x,31,19")
+  val smallInput = List("939", "7,13,x,x,59,x,31,19")
 
   def readInput(input:List[String]) = (input(0).toInt, input(1).split(","))
 
+  // Part 1: find next bus
   def closests(targetTime:Int, busTime:Int) = 
     val before = (targetTime / busTime) * busTime
     val after = before + busTime
@@ -21,7 +21,37 @@ object Day13 {
     wait * busId
   }
 
-  def main(args: Array[String]): Unit = {
-    println("part1=" + findClosests(fullInput))
+  // Part 2: Contest
+  def contestFindTime(input:List[String]) = {
+    val (_, timesStr) = readInput(input)
+
+    val firstBus = timesStr.head.toInt
+    val busTime = timesStr.tail.foldLeft(List[(Long,Long)]((firstBus,0)))((acc,e) => (e, acc) match {
+      case ("x", (-1,c)::tail) => (-1,c+1)::tail
+      case ("x", head::tail) => (-1,1 + head._2)::head::tail
+      case (v, (-1,c)::tail) => (v.toInt,c+1)::tail
+      case (v, head::tail) => (v.toInt, 1 + head._2):: head::tail
+    }).reverse
+
+    def mergeBusTimes(inc1:Long, start1:Long, inc2:Long, start2:Long) : (Long,Long) =
+      val inc = inc1 * inc2
+      val start =
+        (for(i <- 0L to inc1 - 1 if ( (start1 + inc2 * i - start2) % inc1 == 0))
+          yield (inc + start2 - inc2 *i) % inc)(0)
+      (inc, start)
+
+    def mergeBusTimes2(busTime1: (Long,Long), busTime2: (Long,Long)) : (Long,Long) =
+      if (busTime1._2 < busTime2._2)
+        mergeBusTimes(busTime1._1, busTime1._2, busTime2._1, busTime2._2)
+      else
+        mergeBusTimes(busTime2._1, busTime2._2, busTime1._1, busTime1._2)
+
+    val r = busTime.reduce(mergeBusTimes2)
+    r._1 - r._2
   }
+
+
+  def main(args: Array[String]): Unit =
+    println("part1=" + findClosests(fullInput))
+    println("part2=" + contestFindTime(fullInput))
 }
--- src/test/scala/Day13Test.scala
+++ src/test/scala/Day13Test.scala
@@ -11,4 +11,38 @@ class Day13Test {
   @Test def testPart1b(): Unit = {
     assertEquals(2238, findClosests(fullInput))
   }
+
+  @Test def testPart2a(): Unit = {
+    assertEquals(1068781, contestFindTime(smallInput))
+  }
+
+  @Test def testPart2b(): Unit = {
+    val l = List("-1","17,x,13,19")
+    assertEquals(3417, contestFindTime(l))
+  }
+
+  @Test def testPart2c(): Unit = {
+    val l = List("-1","67,7,59,61")
+    assertEquals(754018, contestFindTime(l))
+  }
+
+  @Test def testPart2d(): Unit = {
+    val l = List("-1","67,x,7,59,61")
+    assertEquals(779210, contestFindTime(l))
+  }
+
+  @Test def testPart2e(): Unit = {
+    val l = List("-1","67,7,x,59,61")
+    assertEquals(1261476, contestFindTime(l))
+  }
+
+  @Test def testPart2f(): Unit = {
+    val l = List("-1","1789,37,47,1889")
+    assertEquals(1202161486, contestFindTime(l))
+  }
+
+  @Test def testPart2g(): Unit = {
+    assertEquals(560214575859998L, contestFindTime(fullInput))
+  }
+
 }
