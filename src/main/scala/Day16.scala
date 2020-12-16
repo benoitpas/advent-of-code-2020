2457552399fe6712523b713d6b219c9444d5593b
benoitpas <bpasquereau@gmail.com>
2020-12-16 22:27:02 +0000

    Day 16 part 2

--- src/main/scala/Day16.scala
+++ src/main/scala/Day16.scala
@@ -2,8 +2,7 @@ object Day16 {
   import scala.io.Source
   val fullInput = Source.fromResource("day16-input.txt").getLines.to(LazyList)
 
-
-  val smallInput = List(
+  val smallInput1 = LazyList(
     "class: 1-3 or 5-7",
     "row: 6-11 or 33-44",
     "seat: 13-40 or 45-50",
@@ -15,7 +14,20 @@ object Day16 {
     "7,3,47",
     "40,4,50",
     "55,2,20",
-    "38,6,12").to(LazyList)
+    "38,6,12")
+
+  val smallInput2 = LazyList(
+    "class: 0-1 or 4-19",
+    "row: 0-5 or 8-19",
+    "seat: 0-13 or 16-19",
+    "",
+    "your ticket:",
+    "11,12,13",
+    "",
+    "nearby tickets:",
+    "3,9,18",
+    "15,1,5",
+    "5,14,9")
 
   type Sections = LazyList[LazyList[String]]
 
@@ -29,32 +41,78 @@ object Day16 {
         case (head#::tail,s) => (s#::head)#::tail
     })
 
-    val fieldsRegex = s"(\\w+):\\s+(\\d+)-(\\d+)\\s+or\\s+(\\d+)-(\\d+)".r
+    val fieldsRegex = s"([\\w\\s]+):\\s+(\\d+)-(\\d+)\\s+or\\s+(\\d+)-(\\d+)".r
     val fields = sections(0).map(fieldsRegex.findFirstMatchIn(_) match {
       case Some(m) =>(m.group(1) -> List(
         (m.group(2).toInt, m.group(3).toInt),
         (m.group(4).toInt, m.group(5).toInt)))
     }).toMap
 
-    val nearbyTickets = sections(2).tail.map(_.split(",").map(_.trim.toInt)).toArray
-    (fields, nearbyTickets)
+    def ticketToRow(s:String) = s.split(",").map(_.trim.toInt)
+    val ticket = ticketToRow(sections(1).tail.head)
+
+    val nearbyTickets = sections(2).tail.map(ticketToRow).toArray
+    (fields, nearbyTickets, ticket)
   }
 
+  def checkIntervals(v:Int, intervals: List[(Int,Int)]) = intervals.foldLeft(false)
+    ((acc,interval) => acc || (interval._1 <= v && v <= interval._2))
 
-  def addInvalidValues(tickets : Array[Array[Int]], fields : Map[String, List[(Int,Int)]]) = {
-    def checkFields(v:Int) = fields.values.foldLeft(false)(
+  def checkFields(v:Int, fields : Map[String, List[(Int,Int)]]) = 
+    fields.values.foldLeft(false)(
       (acc,intervals) => ((intervals(0)._1 <= v && v <= intervals(0)._2)
         || (intervals(1)._1 <= v && v <= intervals(1)._2) || acc))
 
+  def addInvalidValues(tickets : Array[Array[Int]], fields : Map[String, List[(Int,Int)]]) = {
     val values = for( 
       row <- tickets;
-      n <- row if (!checkFields(n))) yield n
+      n <- row if (!checkFields(n, fields))) yield n
     
     values.sum
   }
 
+  def mapFields(tickets : Array[Array[Int]], fields : Map[String, List[(Int,Int)]]) = {
+    def checkTicket(ticket:Array[Int]) =
+       ticket.foldLeft(true)((acc,v) => checkFields(v,fields) && acc)
+
+    val goodTickets = tickets.foldLeft(List[Array[Int]]())(
+      (acc, ticket) => if (checkTicket(ticket)) ticket::acc else acc).toArray
+
+    def check(column: IndexedSeq[Int], intervals: List[(Int,Int)]) = 
+      column.foldLeft(true)((acc,v) => checkIntervals(v,intervals) && acc)
+
+    val possibleMapping = (0 to goodTickets(0).length - 1).map((columnIndex) => {
+      fields.flatMap((name,intervals) => {
+        val column = for (i <- (0 to goodTickets.length - 1)) yield goodTickets(i)(columnIndex)
+        if ( check(column, intervals)) List(name) else List()
+      })
+    })
+
+    val iPossibleMapping = possibleMapping.zipWithIndex
+    def loop(iPossibleMapping :IndexedSeq[(Iterable[String], Int)], mapping:Map[String,Int]): Map[String,Int] =
+      if (iPossibleMapping.length == 0) mapping
+      else {
+        val (field, index) = iPossibleMapping.find(_._1.size == 1) match {
+          case Some((field::_, index)) => (field, index)
+        }
+        val newPossibleMapping = iPossibleMapping.map(
+          (fields,index) => (fields.filter(_ != field), index)).filter(_._1.size > 0)
+        loop(newPossibleMapping, mapping + (field -> index))
+      }
+
+    loop(iPossibleMapping, Map[String,Int]())
+    
+  }
+
+  def departureFieldsProduct(tickets: Array[Array[Int]], 
+    fields: Map[String, List[(Int,Int)]], myTicket: Array[Int]) =
+    val allFields = mapFields(tickets, fields)
+    val departureFields = allFields.filter((pair) => "departure .*".r.matches(pair._1))
+    departureFields.values.map(myTicket(_).toLong).reduce(_ * _)
+
   def main(args: Array[String]): Unit = {
-    val r2 = parseInput(fullInput)
-    println("part1=" + addInvalidValues(r2._2,r2._1))
+    val (fields, tickets, myTicket) = parseInput(fullInput)
+    println("part1=" + addInvalidValues(tickets, fields))
+    println("part2=" + departureFieldsProduct(tickets, fields, myTicket))
   }
 }
--- src/test/scala/Day16Test.scala
+++ src/test/scala/Day16Test.scala
@@ -5,14 +5,29 @@ import Day16._
 
 class Day16Test {
   @Test def testPart1a(): Unit = {
-    val r = parseInput(smallInput)
+    val r = parseInput(smallInput1)
     assertEquals(71, addInvalidValues(r._2,r._1))
   }
 
+  val fullInputParsed = parseInput(fullInput)
+
   @Test def testPart1b(): Unit = {
-    val r = parseInput(fullInput)
-    assertEquals(26988, addInvalidValues(r._2,r._1))
+    assertEquals(26988, addInvalidValues(fullInputParsed._2,fullInputParsed._1))
   }
+
+  @Test def testPart2a(): Unit = {
+    val r = parseInput(smallInput2)
+    val mapping = mapFields(r._2,r._1)
+    assertEquals(Map("row" -> 0, "class" -> 1, "seat" -> 2), mapping)
+    assertEquals(List(11,12,13), r._3.toList)
+  }
+
+  @Test def testPart2b(): Unit = {
+    assertEquals(426362917709L, departureFieldsProduct(fullInputParsed._2, fullInputParsed._1, fullInputParsed._3))
+  }
+
+  //
+
 }
 
   
