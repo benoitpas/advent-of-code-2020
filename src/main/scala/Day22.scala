913faeb4ebdd81fec8f34de985f6f733f8291153
Benoit Pasquereau <bpasquereau@gmail.com>
2020-12-22 09:02:53 +0000

    Day 22 part 2

--- src/main/scala/Day22.scala
+++ src/main/scala/Day22.scala
@@ -17,25 +17,68 @@ object Day22 {
   "7",
   "10")
 
+  val verySmallInput = LazyList(
+    "Player 1:",
+    "43",
+    "19",
+    "",
+    "Player 2:",
+    "2",
+    "29",
+    "14")
+
+  type Decks = (List[Int],List[Int])
   @scala.annotation.tailrec
-  def play(cards1:List[Int], cards2:List[Int]) : (List[Int], List[Int]) = 
-    if( cards1.length == 0 || cards2.length == 0) (cards1,cards2)
-    else  math.signum(cards1.head - cards2.head) match {
-      case 1 => play(cards1.tail ++ List(cards1.head,cards2.head), cards2.tail)
-      case -1 => play(cards1.tail, cards2.tail ++ List(cards2.head, cards1.head))
-      case _ => throw Exception("Cards should not be equal")
-    }
+  def play1(cards1:List[Int], cards2:List[Int]) : Decks =
+    if (cards1.length == 0 || cards2.length == 0) (cards1,cards2)
+    else
+      math.signum(cards1.head - cards2.head) match {
+        case 1 => play1(cards1.tail ++ List(cards1.head,cards2.head), cards2.tail)
+        case -1 => play1(cards1.tail, cards2.tail ++ List(cards2.head, cards1.head))
+        case _ => throw Exception("Cards should not be equal")
+      }
  
-  def score(input:LazyList[String]) = 
+  def scoreFromDecks(decks: Decks) =
+    val winningCards = if (decks._2.length > 0) decks._2 else decks._2
+    (winningCards zip (winningCards.length to 1 by -1)).map((p) => p._1 * p._2).sum
+
+  def score1(input:LazyList[String]) =
     val sections = Day16.extractSections(input)
     val cards1 = sections(0).tail.map(_.toInt).toList
     val cards2 = sections(1).tail.map(_.toInt).toList
-    val gameResult = play(cards1,cards2)
+    val decks = play1(cards1,cards2)
+    scoreFromDecks(decks)
 
-    val winningCards = if (gameResult._1.length > 0) gameResult._1 else gameResult._2
-    (winningCards zip (winningCards.length to 1 by -1)).map((p) => p._1 * p._2).sum
+  def recursiveGame(cards1:List[Int], cards2:List[Int]) =
+    cards1.tail.length >= cards1.head && cards2.tail.length >= cards2.head
+
+  def play2(cards1:List[Int], cards2:List[Int], games:Set[Decks]) : Decks =
+    //println(cards1.toString+"\t"+cards2+"\t"+games)
+    lazy val newGames = games ++ Set((cards1,cards2))
+    if (cards1.length == 0 || cards2.length == 0|| games.contains((cards1,cards2))) (cards1,cards2)
+    else
+      val winningDeck =
+        if (recursiveGame(cards1,cards2))
+          play2(cards1.tail.take(cards1.head), cards2.tail.take(cards2.head), newGames) match
+            case (Nil,_) => -1
+            case _ => 1
+        else math.signum(cards1.head - cards2.head)
+
+      winningDeck match {
+        case 1 => play2(cards1.tail ++ List(cards1.head,cards2.head), cards2.tail, newGames)
+        case -1 => play2(cards1.tail, cards2.tail ++ List(cards2.head, cards1.head), newGames)
+        case _ => throw Exception("Cards should not be equal")
+      }
+
+  def score2(input:LazyList[String]) =
+    val sections = Day16.extractSections(input)
+    val cards1 = sections(0).tail.map(_.toInt).toList
+    val cards2 = sections(1).tail.map(_.toInt).toList
+    val decks = play2(cards1,cards2, Set())
+    scoreFromDecks(decks)
 
   def main(args: Array[String]): Unit = {
-   println("part1=" + score(fullInput))
+    println("part1=" + score1(fullInput))
+    println("part2=" + score2(fullInput))
   }
 }
--- src/test/scala/Day22Test.scala
+++ src/test/scala/Day22Test.scala
@@ -4,11 +4,25 @@ import org.junit.Assert._
 import Day22._
 
 class Day22Test {
-  @Test def testSmallInput(): Unit = {
-    assertEquals(306, score(smallInput))
+  @Test def testSmallInput1(): Unit = {
+    assertEquals(306, score1(smallInput))
   }
 
-  @Test def testFullInput(): Unit = {
-    assertEquals(30197, score(fullInput))
+  @Test def testFullInput1(): Unit = {
+    assertEquals(30197, score1(fullInput))
   }
+
+  @Test def testverySmallInput2(): Unit = {
+    assertEquals(78, score2(verySmallInput))
+  }
+
+
+  @Test def testSmallInput2(): Unit = {
+    assertEquals(291, score2(smallInput))
+  }
+
+  @Test def testFullInput2(): Unit = {
+    assertEquals(34031, score2(fullInput))
+  }
+
 }
